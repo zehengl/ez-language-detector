4371ae16eca2a67de35ae8b7f4eee6baebb67f96
Zeheng Li <imzehengl@gmail.com>
2019-10-04 00:29:54 -0600

    detect language

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request
+from iso639 import languages
+from langdetect import detect
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
 
+def get_language(texts):
+    alpha2 = detect(texts).split("-")[0]
+    return languages.get(alpha2=alpha2).name
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         texts = request.form["texts"]
-        language = "English"
+        language = get_language(texts)
         return render_template("index.html", language=language)
     else:
         return render_template("index.html")
