95ad78b81c41dc6edec5b0a1973753d6b308bb06
Zeheng Li <imzehengl@gmail.com>
2019-11-21 11:53:59 -0700

    refresh

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
+import os
+
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 from iso639 import languages
 from langdetect import detect
 from whitenoise import WhiteNoise
 
+from forms import TextInputForm
+
+
 app = Flask(__name__)
+Bootstrap(app)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "SECRET_KEY")
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 
 
 def get_language(texts):
+    if not texts:
+        return None
+
     try:
         alpha2 = detect(texts).split("-")[0]
     except:
@@ -16,14 +28,12 @@ def get_language(texts):
         return languages.get(alpha2=alpha2).name
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["get", "post"])
 def index():
-    if request.method == "POST":
-        texts = request.form["texts"]
-        language = get_language(texts)
-        return render_template("index.html", language=language)
-    else:
-        return render_template("index.html")
+    form = TextInputForm(request.form)
+    language = get_language(form.text_input.data)
+
+    return render_template("index.html", form=form, language=language)
 
 
 if __name__ == "__main__":
